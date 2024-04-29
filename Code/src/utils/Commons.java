4bd150ae87935c50513e782d4b112fc8108b67c7
Reis <80530563+JoaoReis153@users.noreply.github.com>
2024-04-29 23:38:47 +0100

    small changes

--- Code/src/utils/Commons.java
+++ Code/src/utils/Commons.java
@@ -23,7 +23,7 @@ public interface Commons {
 
 	public static final int PACMAN_STATE_SIZE = PacmanBoard.N_BLOCKS * PacmanBoard.N_BLOCKS * 2 + 2 + PacmanBoard.MAX_GHOSTS * 2;
 	public static final int PACMAN_NUM_ACTIONS = 5;
-	public static final int PACMAN_HIDDEN_LAYER = 25;
+	public static final int PACMAN_HIDDEN_LAYER = 20;
 	public static final int PACMAN_NETWORK_SIZE = (PACMAN_STATE_SIZE * PACMAN_HIDDEN_LAYER) + PACMAN_HIDDEN_LAYER +
 			(PACMAN_HIDDEN_LAYER * PACMAN_NUM_ACTIONS) + PACMAN_NUM_ACTIONS;
 
