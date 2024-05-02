699648aafeb11d595599d82d46d17be67d4e5c99
Reis <80530563+JoaoReis153@users.noreply.github.com>
2024-05-02 08:44:59 +0100

    breakout removed and GUI is now visible

--- src/pacman/Main.java
+++ src/pacman/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int seed = 10;
+        int seed = 3113;
         PacmanGeneticAlgorithm ga = new PacmanGeneticAlgorithm(seed);
         PacmanNeuralNetwork nn = ga.search();
         new Pacman(nn, true, nn.getSeed());
