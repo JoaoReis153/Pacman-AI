729f53eb5fdfc2c240914be01b0154265b7a906d
Reis <80530563+JoaoReis153@users.noreply.github.com>
2024-05-14 11:59:01 +0100

    Unused constants removed

--- src/utils/Commons.java
+++ src/utils/Commons.java
@@ -3,26 +3,9 @@ package utils;
 import pacman.PacmanBoard;
 
 public interface Commons {
-    
-	public static final int WIDTH = 300;
-	public static final int HEIGHT = 400;
-	public static final int BOTTOM_EDGE = 390;
-	public static final  int N_OF_BRICKS = 1;//30;
-	public static final int INIT_PADDLE_X = 200;
-	public static final int INIT_PADDLE_Y = 360;
-	public static final int INIT_BALL_X = 230;
-	public static final int INIT_BALL_Y = 355;    
-	public static final int PERIOD = 5;
-    
-	public static final int BREAKOUT_STATE_SIZE = 7;
-	public static final int BREAKOUT_HIDDEN_LAYER = 6;
-	public static final int BREAKOUT_NUM_ACTIONS = 2;
-
-	public static final int BREAKOUT_NETWORK_SIZE = (BREAKOUT_STATE_SIZE * BREAKOUT_HIDDEN_LAYER) + BREAKOUT_HIDDEN_LAYER +
-			(BREAKOUT_HIDDEN_LAYER * BREAKOUT_NUM_ACTIONS) + BREAKOUT_NUM_ACTIONS;
 
 	public static final int PACMAN_STATE_SIZE = PacmanBoard.N_BLOCKS * PacmanBoard.N_BLOCKS * 2 + 2 + PacmanBoard.MAX_GHOSTS * 2;
-	public static final int PACMAN_NUM_ACTIONS = 4;
+	public static final int PACMAN_NUM_ACTIONS = 5;
 	public static final int PACMAN_HIDDEN_LAYER = 12;
 	public static final int PACMAN_NETWORK_SIZE = (PACMAN_STATE_SIZE * PACMAN_HIDDEN_LAYER) + PACMAN_HIDDEN_LAYER +
 			(PACMAN_HIDDEN_LAYER * PACMAN_NUM_ACTIONS) + PACMAN_NUM_ACTIONS;
